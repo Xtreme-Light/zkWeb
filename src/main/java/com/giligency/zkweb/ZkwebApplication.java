49dfe1f05164c3172b9a6744fd5184eb1cb402a0
xuliang <xuliang@asiainfo.com>
2019-10-23 18:03:29 +0800

    init

---
+++ src/main/java/com/giligency/zkweb/ZkwebApplication.java
@@ -0,0 +1,13 @@
+package com.giligency.zkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giligency/zkweb/ZkwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giligency.zkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
